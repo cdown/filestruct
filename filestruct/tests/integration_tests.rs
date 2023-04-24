a11550c34110abe3693f5e8eb87dcb0de8abc62e
Chris Down <chris@chrisdown.name>
2023-04-24 16:43:50 +0100

    tests: Test #[filestruct(file = ...)]

--- filestruct/tests/integration_tests.rs
+++ filestruct/tests/integration_tests.rs
@@ -5,17 +5,29 @@ fn get_test_dir() -> PathBuf {
     Path::new(env!("CARGO_MANIFEST_DIR")).join("test_data")
 }
 
-#[derive(FromDir)]
-struct FPrimitive {
-    t_u64_ok: u64,
-    t_string_ok: String,
-    t_string_ok_chomp: String,
-}
-
 #[test]
-fn struct_basic() {
-    let f = FPrimitive::from_dir(get_test_dir()).unwrap();
+fn bare() {
+    #[derive(FromDir)]
+    struct F {
+        t_u64_ok: u64,
+        t_string_ok: String,
+        t_string_ok_chomp: String,
+    }
+
+    let f = F::from_dir(get_test_dir()).unwrap();
     assert_eq!(f.t_u64_ok, u64::MAX);
     assert_eq!(f.t_string_ok, "ĩ ľ𝝸ᶄ𝙚 ѕ𝓉ř⍳𝕟ℊ𝚜, ṁ𝚎\n");
     assert_eq!(f.t_string_ok_chomp, "ĩ ľ𝝸ᶄ𝙚 ѕ𝓉ř⍳𝕟ℊ𝚜, ṁ𝚎");
+}
+
+#[test]
+fn attr_file() {
+    #[derive(FromDir)]
+    struct F {
+        #[filestruct(file = "t_string_ok")]
+        renamed: String,
+    }
+
+    let f = F::from_dir(get_test_dir()).unwrap();
+    assert_eq!(f.renamed, "ĩ ľ𝝸ᶄ𝙚 ѕ𝓉ř⍳𝕟ℊ𝚜, ṁ𝚎\n");
 }
