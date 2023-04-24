b6091ccfe11dcdfa3ccef53f56f047e1923a4600
Chris Down <chris@chrisdown.name>
2023-04-24 16:15:50 +0100

    tests: Add string test

--- filestruct/tests/integration_tests.rs
+++ filestruct/tests/integration_tests.rs
@@ -8,10 +8,14 @@ fn get_test_dir() -> PathBuf {
 #[derive(FromDir)]
 struct FPrimitive {
     t_u64_ok: u64,
+    t_string_ok: String,
+    t_string_ok_chomp: String,
 }
 
 #[test]
 fn struct_basic() {
     let f = FPrimitive::from_dir(get_test_dir()).unwrap();
     assert_eq!(f.t_u64_ok, u64::MAX);
+    assert_eq!(f.t_string_ok, "ĩ ľ𝝸ᶄ𝙚 ѕ𝓉ř⍳𝕟ℊ𝚜, ṁ𝚎\n");
+    assert_eq!(f.t_string_ok_chomp, "ĩ ľ𝝸ᶄ𝙚 ѕ𝓉ř⍳𝕟ℊ𝚜, ṁ𝚎");
 }
